93490ec42dfb92cfdab087d2bc2c55fdf6cc1daa
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-05-16 14:49:10 +0530

    added

--- L20.js
+++ L20.js
@@ -1,36 +1,128 @@
 
 
 
-// let obj = { a: 1 }
+// // let obj = { a: 1 }
 
-// let obj = { name: "ram" }
-// let ws = new WeakSet()
+// // let obj = { name: "ram" }
+// // let ws = new WeakSet()
 
-// ws.add(obj)
+// // ws.add(obj)
 
-// console.log(ws)
+// // console.log(ws)
 
-// ws.add(obj)
-// ws.has()
-// console.log(ws)
+// // ws.add(obj)
+// // ws.has()
+// // console.log(ws)
 
-let visitedSet = new WeakSet();
+// let visitedSet = new WeakSet();
 
-let john = { name: "John" };
-let pete = { name: "Pete" };
-let mary = { name: "Mary" };
+// let john = { name: "John" };
+// let pete = { name: "Pete" };
+// let mary = { name: "Mary" };
 
-visitedSet.add(john); // John visited us
-visitedSet.add(pete); // Then Pete
-visitedSet.add(john); // John again
+// visitedSet.add(john); // John visited us
+// visitedSet.add(pete); // Then Pete
+// visitedSet.add(john); // John again
 
-// visitedSet has 2 users now
-console.log(visitedSet)
+// // visitedSet has 2 users now
+// console.log(visitedSet)
 
-// // check if John visited?
-// alert(visitedSet.has(john)); // true
+// // // check if John visited?
+// // alert(visitedSet.has(john)); // true
 
-// // check if Mary visited?
-// alert(visitedSet.has(mary)); // false
+// // // check if Mary visited?
+// // alert(visitedSet.has(mary)); // false
 
-// john = null;
+// // john = null;
+
+
+// function reverseString(str) {
+//     // Initialize an empty stack
+//     const stack = [];
+
+//     // Push each character of the string onto the stack
+//     for (let char of str) {
+//         stack.push(char);
+//     }
+//     // console.log(stack)
+
+//     // Pop each character from the stack to construct the reversed string
+//     let reversedStr = '';
+//     while (stack.length > 0) {
+//         reversedStr = reversedStr + stack.pop();
+//     }
+
+//     return reversedStr;
+// }
+
+// // Test the function
+// const originalString = "hello";
+// const reversedString = reverseString(originalString);
+// console.log("Original String:", originalString);
+// console.log("Reversed String:", reversedString);
+
+
+
+// let str = "radheShyam";
+
+// let stack = [];
+
+// for (let i = 0; i < str.length; i++) {
+//     stack.push(str[i])
+// }
+
+// // stack // r a d h e S h y a m  // 9 , 
+
+// let reversedString = ""; // 
+
+// while (stack.length > 0) {
+//     reversedString = reversedString + stack.pop()
+// }
+
+// console.log(reversedString)
+
+
+
+// console.log(stack)
+
+
+
+
+// function isValidParentheses(s) {
+//     // Create a stack to keep track of opening brackets
+//     const stack = [];
+
+//     // Create a map to store mappings of opening and closing brackets
+//     const bracketMap = {
+//         '(': ')',
+//         '[': ']',
+//         '{': '}'
+//     };
+
+//     // Iterate through each character in the string
+//     for (let i = 0; i < s.length; i++) {
+//         const char = s[i];
+
+//         // If the character is an opening bracket, push it onto the stack
+//         if (bracketMap[char]) {
+//             stack.push(char);
+//         } else {
+//             // If the character is a closing bracket
+//             // Check if the stack is empty or if the top of the stack does not match the current closing bracket
+//             if (stack.length === 0 || bracketMap[stack.pop()] !== char) {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     // If the stack is empty after iterating through all characters, the string is valid
+//     return stack.length === 0;
+// }
+
+// // Test the function
+// const testString1 = "()[]{}"; // Valid string
+// const testString2 = "([)]"; // Invalid string
+// console.log(testString1, "is valid:", isValidParentheses(testString1));
+// console.log(testString2, "is valid:", isValidParentheses(testString2));
+
+
---
+++ L21.js
@@ -0,0 +1,196 @@
+function Stack() {
+    const stack = [];
+    let overflow = false;
+
+    // Push operation
+    function push(item) {
+        if (stack.length >= 5) {
+            overflow = true;
+            console.log("Stack Overflow!");
+        } else {
+            stack.push(item);
+            overflow = false;
+        }
+    }
+
+    // Pop operation
+    function pop() {
+        if (stack.length === 0) {
+            console.log("Stack Underflow!");
+        } else {
+            stack.pop();
+        }
+    }
+
+    // Peek operation
+    function peek() {
+        if (stack.length === 0) {
+            console.log("Stack is empty");
+        } else {
+            console.log("Top element of stack:", stack[stack.length - 1]);
+        }
+    }
+
+    // Display the stack
+    function display() {
+        console.log("Stack:", stack);
+    }
+
+    return { push, pop, peek, display, overflow };
+}
+
+// Example usage
+const myStack = Stack();
+myStack.push(10);
+myStack.push(20);
+myStack.push(30);
+myStack.push(40);
+myStack.push(50);
+myStack.push(60); // This should trigger stack overflow
+myStack.display();
+myStack.pop();
+myStack.peek();
+
+
+
+
+
+// function Stack() {
+//     const stack = [];
+//     let overflow = false;
+
+//     // push operation 
+
+//     function push(item) {
+//         if (stack.length >= 5) {
+//             overflow = true;
+//             console.log("stack Overflow ")
+//         } else {
+//             stack.push(item)
+//             overflow = false
+//         }
+//     }
+
+
+//     // pop operation 
+//     function pop() {
+//         if (stack.length == 0) {
+//             console.log("stack is underflow")
+//         } else {
+//             stack.pop()
+//         }
+//     }
+
+//     // peek opetation
+//     function peek() {
+//         if (stack.length === 0) {
+//             console.log('stack is empty')
+//         } else {
+//             console.log("top element is :", stack[stack.length - 1])
+//         }
+//     }
+
+//     // display the stack
+
+//     function display() {
+//         console.log(stack, "stack")
+//     }
+
+//     return { push, pop, peek, display, overflow }
+
+// }
+
+
+// const myyStack = Stack()
+// myStack.push(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////s
+// console.log("jai ram")
+// console.log("har har mahadev")
+
+
+
+
+
+//add element
+
+// func
+
+
+
+// function stackMethods() {
+
+//     let stack = [];
+
+//     function Add(ele) {
+//         if (stack.length > 2) {
+//             console.log("stack overflow")
+//         } else {
+//             stack.push(ele)
+//             console.log(stack)
+//         }
+//     }
+
+//     //delete
+
+//     function Delete() {
+//         if (stack.length == 0) {
+//             console.log("Stack is under flow")
+//         } else {
+//             stack.pop();
+//             console.log(stack)
+//         }
+//     }
+    
+//     // peek
+//     function Peek() {
+//         if (stack.length === 0) {
+//             console.log("Stack is underFlow")
+//         } else {
+//             console.log(stack[stack.length - 1])
+//             console.log(stack)
+//         }
+        
+//     }
+    
+//     return { Add, Delete , Peek}
+
+// }
+
+// let mainFun = stackMethods()
+// mainFun.Add(10)
+// mainFun.Add(20)
+// mainFun.Add(40)
+// mainFun.Add(20)
+// mainFun.Peek()
+// mainFun.Delete()
+// mainFun.Delete()
+
