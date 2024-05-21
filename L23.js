998cb3adb899d6d1ae2898abf9481fc25bb5145b
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-05-21 10:41:21 +0530

    added

--- L23.js
+++ L23.js
@@ -65,3 +65,84 @@ console.log(queue.size()); // 3
 console.log(queue.peek()); // 10
 queue.dequeue();
 console.log(queue.print()); // 20 30
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Createqueue() {
+
+    const items = [];
+
+    // enqueue : Add an element to rear of the queue
+
+    function enque(ele) {
+        items.push(ele)
+    }
+
+    // deque : remove an element from the front 
+    function deque() {
+        if (items.length === 0) {
+            return " underflow"
+        } else {
+            return items.shift()
+        }
+    }
+
+    // peake element
+    function peek() {
+        if (items.length == 0) {
+            return "queue is empty"
+        } else {
+            console.log(items[0], "peek element")
+        }
+
+    }
+
+
+
+    function size() {
+        console.log(items.length, "size")
+    }
+
+    function print() {
+        let str = ""
+        for (let i = 0; i < items.length; i++) {
+            str += items[i] + " "
+        }
+        console.log(str)
+    }
+
+
+    return { enque, print, deque, peek, size }
+
+}
+
+
+
+let myFun = Createqueue();
+
+myFun.enque(3)
+myFun.enque(4)
+myFun.enque(5)
+
+myFun.deque()
+
+myFun.peek()
+
+myFun.size()
+
+
+
+
+
+
+myFun.print()
